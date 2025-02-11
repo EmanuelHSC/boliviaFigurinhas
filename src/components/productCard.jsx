5d48513b9a615326763b1e3c1de8e5e743f242cb
EmanuelHSC <emanuelhoffmann0@hotmail.com>
2025-02-11 19:16:13 -0300

    feat(productCard and productList): add tela de visualização de componente principal de exibir as cartas a venda

---
+++ src/components/productCard.jsx
@@ -0,0 +1,84 @@
+// src/components/productCard.jsx
+
+import React, { useState } from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import PropTypes from "prop-types";
+import Button from "@mui/material/Button";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { useDispatch } from "react-redux";
+import { Creators as CartActions } from "../store/ducks/cart";
+import Snackbar from "@mui/material/Snackbar";
+import Alert from "@mui/material/Alert";
+
+function ProductCard({ name = "Default Name", description = "Default Description", image, price = "0.00", id, quantity = 0 }) {
+  const dispatch = useDispatch();
+  const [openAlert, setOpenAlert] = useState(false);
+  const vertical = "bottom";
+  const horizontal = "center";
+
+  const handleClickAlert = () => {
+    setOpenAlert(true);
+  };
+
+  const handleCloseAlert = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpenAlert(false);
+  };
+
+  const handleAddToCart = () => {
+    dispatch(CartActions.addToCart({ name, description, image, price, id }));
+    handleClickAlert();
+  };
+
+  return (
+    <Card sx={{ width: 350, height: 480, m: 2, display: "flex", flexDirection: "column", justifyContent: "space-between" }}>
+      <CardMedia
+        component="img"
+        sx={{
+          height: 250,
+          objectFit: "contain",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: 2,
+        }}
+        image={image || "https://via.placeholder.com/150"}
+        alt={name}
+      />
+      <CardContent sx={{ flexGrow: 1, display: "flex", flexDirection: "column", justifyContent: "space-between", padding: 3 }}>
+        <Typography gutterBottom variant="h3" component="div" sx={{ textAlign: "center", fontWeight: "bold", fontSize: "1.2rem" }}>
+          {name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary" sx={{ overflow: "hidden", textOverflow: "ellipsis", display: "-webkit-box", WebkitLineClamp: 4, WebkitBoxOrient: "vertical" }}>
+          {description}
+        </Typography>
+        <Typography variant="body1" color="text.primary" sx={{ fontWeight: "bold", textAlign: "right", mt: 2, fontSize: "1.1rem" }}>
+          R${price} — Qtd: {quantity}
+        </Typography>
+        <Button variant="contained" sx={{ marginTop: 2, alignSelf: "center", width: "80%", height: 40 }} onClick={handleAddToCart}>
+          <ShoppingCartIcon />
+        </Button>
+      </CardContent>
+      <Snackbar open={openAlert} autoHideDuration={6000} onClose={handleCloseAlert} anchorOrigin={{ vertical, horizontal }}>
+        <Alert onClose={handleCloseAlert} severity="success" variant="filled" sx={{ width: "100%" }}>
+          Produto adicionado ao carrinho!
+        </Alert>
+      </Snackbar>
+    </Card>
+  );
+}
+
+ProductCard.propTypes = {
+  name: PropTypes.string,
+  description: PropTypes.string,
+  image: PropTypes.string,
+  price: PropTypes.string,
+  quantity: PropTypes.number,
+};
+
+export default ProductCard;
---
+++ src/views/productList.jsx
@@ -0,0 +1,89 @@
+// src/views/productList.jsx
+
+import React, { useState, useEffect } from "react";
+import ProductCard from "../components/productCard";
+import Card from "@mui/material/Card";
+import Grid from "@mui/material/Grid";
+import { styled } from "@mui/material/styles";
+import PropTypes from "prop-types";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Pagination from "@mui/material/Pagination";
+import { useLocation } from "react-router-dom";
+import api from "../api/api";
+
+const StyledCard = styled(Card)({
+  height: "calc(100vh - 64px)",
+  overflowY: "auto",
+  boxShadow: "none",
+  backgroundColor: "#f5f5f5",
+});
+
+function ProductList() {
+  const [products, setProducts] = useState([]);
+  const location = useLocation();
+
+  useEffect(() => {
+    const loadProducts = async () => {
+      try {
+        const response = await api.get("/products");
+        setProducts(response.data);
+      } catch (error) {
+        console.error("Erro ao carregar produtos:", error);
+      }
+    };
+
+    loadProducts();
+  }, [location]);
+
+  const [page, setPage] = useState(1);
+  const itemsPerPage = 8;
+
+  const startIndex = (page - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentItems = products.slice(startIndex, endIndex);
+
+  const handleChange = (event, value) => {
+    setPage(value);
+  };
+
+  return (
+    <StyledCard>
+      <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", padding: 2 }}>
+        <Typography variant="h3" component="h2" align="center" fontWeight="bold">
+          Produtos
+        </Typography>
+      </Box>
+      <Grid container spacing={2} sx={{ padding: 2 }}>
+        {currentItems.map((product) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} key={product.id}>
+            <ProductCard
+              name={product.name}
+              description={product.description}
+              image={product.image}
+              price={product.price}
+              id={product.id}
+              quantity={product.quantity}
+            />
+          </Grid>
+        ))}
+      </Grid>
+      <Box sx={{ display: "flex", justifyContent: "center", p: 2 }}>
+        <Pagination count={Math.ceil(products.length / itemsPerPage)} page={page} onChange={handleChange} color="primary" />
+      </Box>
+    </StyledCard>
+  );
+}
+
+ProductList.propTypes = {
+  products: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      description: PropTypes.string,
+      image: PropTypes.string,
+      price: PropTypes.string,
+    })
+  ),
+};
+
+export default ProductList;
